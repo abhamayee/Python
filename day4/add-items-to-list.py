dc753fb8552e6c761bdf4378511d35f373346375
abhamayee <106445682+abhamayee@users.noreply.github.com>
2024-10-05 15:38:02 +0200

    Add items into list

---
+++ day4/add-items-to-list.py
@@ -0,0 +1,16 @@
+# append
+L = [1, 2, 3, 4, 5]
+L.append('5.6')  # add one item to the end
+print(L)
+L.append([9,10])
+print(L)
+# extend
+L.extend([6, 7, 8])  # add multiple items to the end
+print(L)
+L.extend('delhi')
+print(L)
+# insert
+L = [1, 2, 3, 4, 5]
+L.insert(1, 100)
+print(L)  # add value to the index location
+
