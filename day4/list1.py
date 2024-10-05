ee79b3d039688bf6ab5aed103c9cc9670d47d5c6
abhamayee <106445682+abhamayee@users.noreply.github.com>
2024-10-05 15:15:38 +0200

    create list

---
+++ day4/list1.py
@@ -0,0 +1,24 @@
+# Create list
+# empty list
+print([])
+# 1D list
+print([1, 2, 3, 4])
+# 2D
+print([1, 2, [3, 4]])
+# 3D list
+print([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+# Heterogeneous
+print([1, True, 5.6, 5+6j, 'Hello'])
+# Using type conversion
+print(list('hello'))
+# indexing
+L= [1, 2, 3, 4, 5]
+# positive
+print(L[0])
+# negative
+print(L[-1])
+# Slicing
+print(L[0:3])
+print(L[-4:-1])
+print(L[::-1])  # reverse the list
+
